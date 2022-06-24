455c0044214fecd7c5e1d2a1a3bae9b2e4b5d8d9
ucyo <cayoglu@me.com>
2022-06-24 13:47:19 +0200

    Add README and harmonize

---
+++ README.md
@@ -0,0 +1,5 @@
+# Squash Compression Library
+
+A library for compression software in Rust with focus on scientific data compression.
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! # Squash Compression Library (sqsh)
+//! # Squash Compression Library
 //!
 //! A library for compression software in Rust with focus on scientific data compression.
 
