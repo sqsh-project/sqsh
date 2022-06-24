5050e86332d705751416c257023ba352ff7f1853
ucyo <cayoglu@me.com>
2022-06-24 13:42:31 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! # Squash Compression Library (sqsh)
+//!
+//! A library for compression software in Rust with focus on scientific data compression.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
