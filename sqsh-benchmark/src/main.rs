e3bc9a2eb86a0d5caf1804578800680e3d39c9e8
ucyo <cayoglu@me.com>
2022-08-23 18:16:05 +0200

    First working parsing for benchmarking with hyperfine

--- sqsh-benchmark/src/main.rs
+++ sqsh-benchmark/src/main.rs
@@ -1,3 +1,11 @@
+use std::{fs::File, io::Read};
+use toml::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "./sqsh-benchmark/examples/base.toml";
+    let mut f = File::open(filename).unwrap();
+    let mut content = String::new();
+    f.read_to_string(&mut content).unwrap();
+    let value = content.as_str().parse::<Value>().unwrap();
+    println!("{value:?}");
 }
