104c6528505a26e820ca7d4ad43174cc7d47844e
ucyo <cayoglu@me.com>
2022-08-27 18:03:53 +0200

    Generate hyperfine parameters from Run

--- sqsh-benchmark/src/main.rs
+++ sqsh-benchmark/src/main.rs
@@ -1,23 +1,22 @@
 #![allow(dead_code)]
 use serde::Deserialize;
-use std::{collections::HashMap, fs::File, io::Read, process::Command as PCommand};
+use std::{collections::HashMap, fs::File, io::Read};
 mod cli;
 use clap::Parser;
 
 fn main() {
     let config = cli::Cli::parse();
     println!("{config:?}");
+
     let mut f = File::open(config.config).unwrap();
     let mut content = String::new();
     f.read_to_string(&mut content).unwrap();
     let value = content.as_str();
-    // let value = value.parse::<Value>().unwrap();
+
     let c: Benchmark = toml::from_str(value).unwrap();
-    println!("{c:?}");
-    let b = c.run.get("past").unwrap().to_cmd_string();
-    println!("{b:?}");
-    let d = c.to_cmd_string();
-    println!("{d:?}");
+    for (_, v) in c.run {
+        println!("{:?}", v.to_hyperfine_params())
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -28,12 +27,53 @@ struct Benchmark {
     run: HashMap<String, Run>,
 }
 
-
 #[derive(Deserialize, Debug)]
 struct Run {
-    command: String,
     commits: Option<Vec<String>>,
     cleanup: Option<String>,
     prepare: Option<String>,
     setup: Option<String>,
+    command: String,
+}
+
+impl Run {
+    fn to_hyperfine_params(&self) -> Vec<String> {
+        let mut result: Vec<String> = Vec::new();
+        match &self.commits {
+            Some(ids) => {
+                result.push("--parameter-list".to_string());
+                result.push("commit".to_string());
+                result.push(ids.join(","));
+            }
+            None => (),
+        }
+        match &self.cleanup {
+            Some(cmd) => {
+                result.push("--cleanup".to_string());
+                result.push(cmd.clone());
+            }
+            None => (),
+        }
+        match &self.prepare {
+            Some(cmd) => {
+                result.push("--prepare".to_string());
+                result.push(cmd.clone());
+            }
+            None => (),
+        }
+        match (&self.setup, &self.commits) {
+            (Some(scmd), Some(_)) => {
+                result.push("--setup".to_string());
+                let concat = format!("git checkout {{commit}} && {scmd}");
+                result.push(concat);
+            }
+            (None, Some(_)) => {
+                result.push("--setup".to_string());
+                result.push("git checkout {commit}".to_string());
+            }
+            _ => (),
+        }
+        result.push(self.command.clone());
+        result
+    }
 }
