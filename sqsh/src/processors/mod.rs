e1d3a4f809dff565e97a90f87ffd6ae475f4a0b0
ucyo <cayoglu@me.com>
2022-09-13 09:16:57 +0200

    Add Adler32 implementation

--- sqsh/src/processors/mod.rs
+++ sqsh/src/processors/mod.rs
@@ -1,9 +1,11 @@
 //! # Processors
-//! 
+//!
 //! Processors are consuming the data stream from the source and writing
 //! some output to the sink. All submodules are implementing some kind of
 //! processors which implement the `crate::core::Process` trait.
+mod adler32;
 mod duplicate;
 
 // Reexport processors on this level
+pub use adler32::Adler32;
 pub use duplicate::Duplicate;
