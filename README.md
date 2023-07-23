c3ccafef034f00ef712251e0b9e1190b5831d4d4
ucyo <cayoglu@me.com>
2023-07-23 11:08:38 +0200

    Update library and pick up slack

--- README.md
+++ README.md
@@ -4,6 +4,4 @@
 # Squash Compression Library
 
 A library for compression software in Rust with focus on scientific data compression.
-Currently the library is under private development. It will be released Q4 2022.
-
-🤿
+Currently the library is under private development. It will be released Q4 2023.
--- sqsh/src/lib.rs
+++ sqsh/src/lib.rs
@@ -1,9 +1,8 @@
 //! # Squash Compression Library
 //!
 //! A library for compression software in Rust with focus on scientific data.
-//! Currently the library is under private development. It will be released Q4 2022.
+//! Currently the library is under private development. It will be released Q4 2023.
 //!
-//! 🤿
 
 pub mod core;
 pub mod processors;
